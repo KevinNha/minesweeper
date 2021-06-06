5c023918d9d7f3cd5d9c62ba0c7d08b3e8dd72e0
Hyunjin Nha <hyunjinnha@gmail.com>
2021-06-05 22:04:30 -0700

    Worked on routing the different difficulties

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import requests
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return app.send_static_file('index.html')
+    if request.method == 'POST':
+        try:
+            game_difficulty = request.form['difficulty_button']
+            return redirect(f'play_{game_difficulty}')
+        except:
+            return "Something went wrong!"
+    else:
+        return render_template('index.html')
 
-@app.route('/choose_difficulty')
-def choose_difficulty():
-    return redirect('/')
+@app.route('/play_<difficulty>')
+def choose_difficulty(difficulty):
+    data_body = {
+        "difficulty" : difficulty,
+    }
+    return render_template('game.html', data_body=data_body)
 
 if __name__ == '__main__':
     app.run(debug=True)
