9a50701b739e22f33a5fcb257cc097f1c7e05e0a
Hyunjin Nha <hyunjinnha@gmail.com>
2021-07-03 16:09:09 -0700

    Toggle flag icon with right clicks.

--- static/js/script.js
+++ static/js/script.js
@@ -9,20 +9,34 @@ function handleClick(index, event) {
     } else if (event.button == 2) {
         data["button"] = "right"
     }
-    sendPost(data)
+    updateSquare(data)
 }
 
-function sendPost(data) {
-    $.ajax({
-        type: 'POST',
-        url: '/postMoveData',
-        contentType: 'application/json',
-        data: JSON.stringify(data),
-        success: function(return_data) {
-            console.log(return_data)
-        },
-        error: function() {
-            alert('error sending data');
+function updateSquare(data) {
+    var square = data["index"];
+    var click_event = data["button"];
+    var game_square = $( "#game_square_" + square)
+
+    if (click_event == "right") {
+        if (game_square.hasClass("game_square")) {
+            game_square.removeClass()
+            game_square.addClass("flag_square")
+        } else {
+            game_square.removeClass()
+            game_square.addClass("game_square")
         }
-    });
+    }
+
+    // $.ajax({
+    //     type: 'POST',
+    //     url: '/postMoveData',
+    //     contentType: 'application/json',
+    //     data: JSON.stringify(data),
+    //     success: function(return_data) {
+    //         console.log(return_data)
+    //     },
+    //     error: function() {
+    //         alert('error sending data');
+    //     }
+    // });
 }
